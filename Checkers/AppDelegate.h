04d09b798aa1a50d69588e055d3b22f876f3ea7f
Baris Karalar <baris.karalar@doa.com.tr>
2014-12-01 11:43:44 +0200

    Initial Commit

---
+++ Checkers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Checkers
+//
+//  Created by DOA Software Mac on 01/12/14.
+//  Copyright (c) 2014 Baro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
